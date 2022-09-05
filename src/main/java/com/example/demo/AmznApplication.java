57340ed63da1c46e2940e6e4cbbd00133abcd860
java <74448030+thinkcodegravity@users.noreply.github.com>
2022-09-05 11:05:54 -0500

    javascript jquery json

--- src/main/java/com/example/demo/AmznApplication.java
+++ src/main/java/com/example/demo/AmznApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -9,6 +11,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class AmznApplication {
 
 	public static void main(String[] args) {
+		ArrayList<String> names=new ArrayList<String> ();
 		SpringApplication.run(AmznApplication.class, args);
 	}
 
--- src/main/webapp/test.js
+++ src/main/webapp/test.js
@@ -4,45 +4,42 @@ var d=10.0;
 var e="john";
 var f='a';
 test();
-function checkUser(){
-	var uid=$("#userid").val(); // READING value from userid text box userid
-	if(uid=="" ){
-		$("#userid").val("MANDATORY"); // WRITE value from userid text box userid
-	}	
-	else if(uid=="john" || uid=="jane")
-	{
-		document.getElementById("userid").style="background:red";
-		alert("userid is taken");
-	}
-	else{
-		document.getElementById("userid").style="background:white";
-		
-	}
-}
-function validate(){
-//	var uid=document.getElementById("userid").value;
-	var uid=$("#userid").val();
+function checkLoginpage(){
+	var uid=document.getElementById("userid").value;
+	// alternative, easier way of coding javascript programs
+	//var uid=$("#userid").val();
 	
-//	var pwd=document.getElementById("password").value;
-	var pwd=$(".password").val();
-	if(uid==""){
+	var pwd=document.getElementById("pass").value;
+	if(uid=="" )
+	{
+		alert("userid is mandatory");
 		document.getElementById("userid").style="background:red";
 		return false;
 	}
-	else if(pwd==""){
+	if(pwd=="" )
+	{
 		alert("password is mandatory");
-		return false;// customer stay on same page
+		document.getElementById("pass").style="background:red";
+		return false;
 	}
-	else{
-		return true; // will make server journey	
-	}
+	else
+		return true;
+
 }
- 
+function testblur(){
+	var uid=document.getElementById("userid").value;
+	if(uid.length < 6 || uid.length > 30)
+	{
+	//	alert("userid must be atleast 6 and less than 30 charcters long");
+	}
+	
+	console.log("customer lost focus from uid text box");
+}
 function test(){
 	var g=10;
 	g=true;
 	var names=["john","jane","andy"];
-	console.log(names[0]);
+	console.log(names[2]);
 	
 	
 	var student={
@@ -90,7 +87,7 @@ function test(){
 	
 	
 	try{
-		var x=n/0;
+		var x=4/2;
 	}catch(err){
 		console.log('catch blcok');
 	}
@@ -99,6 +96,40 @@ function test(){
 	}
 	
 }
+function checkUser(){
+	var uid=$("#userid").val(); // READING value from userid text box userid
+	if(uid=="" ){
+		$("#userid").val("MANDATORY"); // WRITE value from userid text box userid
+	}	
+	else if(uid=="john" || uid=="jane")
+	{
+		document.getElementById("userid").style="background:red";
+		alert("userid is taken");
+	}
+	else{
+		document.getElementById("userid").style="background:white";
+		
+	}
+}
+function validate(){
+//	var uid=document.getElementById("userid").value;
+	var uid=$("#userid").val();
+	
+//	var pwd=document.getElementById("password").value;
+	var pwd=$(".password").val();
+	if(uid==""){
+		document.getElementById("userid").style="background:red";
+		return false;
+	}
+	else if(pwd==""){
+		alert("password is mandatory");
+		return false;// customer stay on same page
+	}
+	else{
+		return true; // will make server journey	
+	}
+}
+ 
 function add(a, b){
 	return a+b;
 }
@@ -186,17 +217,37 @@ function disableReg (){
 }
 function manipulateDom(){
 	var tableCode=" <table border='1'> <tr><td>apple</td><td>mango</td></tr></table>"
-	$("#footer").html(tableCode);
+//	$("#footer").html(tableCode);
 }
 $(document).ready(function() {
-		$(".password").blur( disableReg );
-		$("#products").click( manipulateDom)
-	//$("#userid").blur( checkUserAjax );
-	// HTML Eleemt)
-//	$("input").click( testInput );
-//	$("#header").mouseenter( headermouse );
-//	$("#header").mouseleave( headermouseout );
-	
+		$("#pass").click ( testingpwd );
+		$("#userid").blur( checkLoginInfo );
+		$("#pass").blur( checkLoginInfo );
 });
 
+function checkLoginInfo(){
+	var uid=$("#userid").val();
+	var pwd=$("#pass").val();
+	if(uid=="" ){
+		$("#loginButton").slideUp( 5000  );// 5 sec= 5000 milli secconds
+		$("#userid").css("background","red");
+	}
+	else if(pwd=="" ){
+		$("#loginButton").slideUp( 5000  );// 5 sec= 5000 milli secconds
+		
+	}
+	else{
+		$("#loginButton").slideDown( 5000 );
+	}
+}
 
+function testingpwd(){
+// READ customer typed value from userid textbox
+	var uid=$("#userid").val();
+	if(uid=="")
+	{
+	// WRITE message in the userid textbox
+		$("#userid").val("MANDATORY");
+	}
+	console.log("customer focused on pwd text box");
+}
