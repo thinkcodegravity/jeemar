e8838679a562f821bd239951e5ac8467a50c50cc
java <74448030+thinkcodegravity@users.noreply.github.com>
2022-09-14 10:01:42 -0500

    notes

---
+++ src/main/java/com/example/demo/Accountant.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Accountant {
+	public static void main(String argsp[]) {
+		Calculator c=new Calculator ();
+		// consuming service
+		c.add(10, 20);
+	}
+}
---
+++ src/main/java/com/example/demo/Calculator.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class Calculator {
+	// providing add service
+	public int add(int a, int b) {
+		return a+b;
+	}
+}
